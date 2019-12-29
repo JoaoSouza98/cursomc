51245e3267c79667a66eb4b0705becaa0db70ddb
Joao Souza <joao_suza@outlook.com>
2019-12-29 12:04:16 -0300

    Endpoint /clientes{id} disponivel

--- src/main/java/com/nelioalves/cursomc/domain/Estado.java
+++ src/main/java/com/nelioalves/cursomc/domain/Estado.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Estado implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -47,6 +49,7 @@ public class Estado implements Serializable {
 		this.nome = nome;
 	}
 
+	@JsonBackReference
 	public List<Cidade> getCidades() {
 		return cidades;
 	}

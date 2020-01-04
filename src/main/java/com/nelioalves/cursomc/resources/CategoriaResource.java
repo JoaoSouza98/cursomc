c3f6f03e8666c05409edbd8b6a35641a53c78fb6
Joao Souza <joao_suza@outlook.com>
2020-01-04 17:40:31 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,18 @@
 package com.nelioalves.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.services.CategoriaService;
@@ -20,8 +24,8 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) { //Response entity encapsula/armazena varias informacoes de uma resposta http para um servico REST
+	@GetMapping(value="/{id}") 
+	public ResponseEntity<?> find(@PathVariable("id") Integer id) { //Response entity encapsula/armazena varias informacoes de uma resposta http para um servico REST
 		
 		Categoria obj = service.buscar(id);
 		
@@ -36,5 +40,21 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		
+		obj = service.insert(obj);
+		
+		URI uri = buildURI(obj, "id", obj.getId());
+		
+		return ResponseEntity.created(uri).build();
+	}
 	
+	private URI buildURI(Object obj, String pathVariable, int attr) {
+		return ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path(String.format("/{%s}", pathVariable))
+				.buildAndExpand(attr)
+				.toUri();
+	}
 }

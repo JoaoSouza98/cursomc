ef2291264cf3a83aeb002afe5d052b5f82710227
Joao Souza <joao_suza@outlook.com>
2020-01-05 17:31:06 -0300

    Listando todas categorias com DTO

---
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,42 @@
+package com.nelioalves.cursomc.dto;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.nelioalves.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public static List<CategoriaDTO> fromList(List<Categoria> list) {
+		return list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+	}
+}

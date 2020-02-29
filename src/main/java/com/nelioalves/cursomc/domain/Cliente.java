77faa3e0fffff7e4a757f8dce07260f4e9f49ad4
Joao Souza <joao_suza@outlook.com>
2020-02-29 10:39:03 -0300

    Permite delecao em cascata de enderecos de um cliente

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -29,7 +30,7 @@ public final class Cliente implements Serializable {
 	public final String cpfOuCnpj;
 	public final Integer tipo;
 		
-	@OneToMany(mappedBy="cliente")
+	@OneToMany(mappedBy="cliente", cascade = CascadeType.ALL)
 	public final List<Endereco> enderecos;
 	
 	@ElementCollection
